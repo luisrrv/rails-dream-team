827f5a73135d222e47514a1260cb392c7c895813
luis rodriguez <rodluisr@gmail.com>
2022-05-15 23:12:29 +0900

    bugfixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pages/home'
   root to: 'teams#index'
   resources :teams, only: [:index, :show, :new, :create] do
     resources :signings, only: [:new, :create]
