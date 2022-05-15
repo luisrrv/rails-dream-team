050de5a79a2198d6515507387a3ec5984a8bae89
luis rodriguez <rodluisr@gmail.com>
2022-05-15 23:46:59 +0900

    bugfix

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ApplicationRecord
   has_many :signings
   has_many :teams
   validates :name, presence: true, uniqueness: true
-  validates :position, presence: true
+  # validates :position, presence: true
 end
